4a11b771988fe9eb5f53bc732cabb078318f90e9
erengc <erenngencturk@gmail.com>
2025-06-16 15:06:35 +0300

    v0.10 Boostable Added

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerInteractionController.cs
@@ -1,13 +1,26 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    private PlayerController _playerController;
+
+    private void Awake()
+    {
+        _playerController = GetComponent<PlayerController>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent<ICollectible>(out var collectible))
         {
             collectible.Collect();
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.TryGetComponent<IBoostable>(out var boostable))
+        {
+            boostable.Boost(_playerController);
         }
     }
 }
