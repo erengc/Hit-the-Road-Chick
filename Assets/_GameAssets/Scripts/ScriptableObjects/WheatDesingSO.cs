f7d4a85c9e8443a0a6b16b81da89c97af33e95eb
erengc <erenngencturk@gmail.com>
2025-06-18 10:30:35 +0300

    v0.11 Some UI Elements Added

--- Assets/_GameAssets/Scripts/ScriptableObjects/WheatDesingSO.cs
+++ Assets/_GameAssets/Scripts/ScriptableObjects/WheatDesingSO.cs
@@ -6,7 +6,15 @@ public class WheatDesingSO : ScriptableObject
 {
     [SerializeField] private float _increaseDecreaseMultiplier;
     [SerializeField] private float _resetBoostDuration;
+    [SerializeField] private Sprite _activeSprite;
+    [SerializeField] private Sprite _passiveSprite;
+    [SerializeField] private Sprite _passiveWheatSprite;
+    [SerializeField] private Sprite _activeWheatSprite;
 
     public float IncreaseDecreaseMultiplier => _increaseDecreaseMultiplier;
     public float ResetBoostDuration => _resetBoostDuration;
+    public Sprite ActiveSprite => _activeSprite;
+    public Sprite PassiveSprite => _passiveSprite;
+    public Sprite ActiveWheatSprite => _activeWheatSprite;
+    public Sprite PassiveWheatSprite => _passiveWheatSprite;
 }
